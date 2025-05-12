3e133df35ae216adb495bdfd4f975bd99b74caf5
Damian Valenzuela Negrete <dvnegrete@gmail.com>
2025-05-12 15:26:09 -0600

    refactor: architecture libraries and log of date

--- src/app/api/viewFile/route.ts
+++ src/app/api/viewFile/route.ts
@@ -1,8 +1,8 @@
 import { NextResponse } from 'next/server';
-import { getCloudStorageGcp } from '@/app/libs/CloudStorageGcp';
+import { getCloudStorageGcp } from '@/libs/CloudStorageGcp';
 
 export async function GET() {
     const response = await getCloudStorageGcp();
+    console.log('Server time:', new Date().toISOString());
     return NextResponse.json(response);
-
 }
